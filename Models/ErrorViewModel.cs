6314581db07e096e67e77666a69321ec35437ed3
CharlyMora <morandres.carlos@gmail.com>
2021-02-20 19:34:00 -0500

    base commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HHRRSupport.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
